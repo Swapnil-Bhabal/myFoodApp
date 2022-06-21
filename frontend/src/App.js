8dedbcab7059926e6dbeeb76c948d652ba1c9d99
Swapnil Bhabal <swapnilbhabal787@gmail.com>
2022-06-21 10:50:46 +0530

    added the data which is required.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import foodList from "./data/foodList";
 
 const App = () => {
   return (
     <>
       <Header/>
+      
       <Footer/>
     </>
   )
---
+++ frontend/src/data/foodList.js
@@ -0,0 +1,94 @@
+const foodList = [
+    {
+        _id: 1,
+        name: 'Summer Watermelon Gazpacho',
+        description: 'This refreshing watermelon gazpacho is the perfect soup for summer.',
+        chef: 'France C',
+        image: '/images/food1.jpg',
+        rating: 3.5,
+        numReviews: 10
+    },
+    {
+        _id: 2,
+        name: 'Chicken Empanadas',
+        description: 'Chef John shows us how to make his baked chicken empanadas',
+        chef: 'Chef John',
+        image: '/images/food2.jpg',
+        rating: 5,
+        numReviews: 2
+    },
+    {
+        _id: 3,
+        name: 'Banana and Vanilla Cupcakes with Buttercream Frosting',
+        description: 'Cupcakes are all the rage.  Here is a simple recipe that I love to use.',
+        chef: 'Alimay',
+        image: '/images/food3.jpg',
+        rating: 4,
+        numReviews: 62
+    },
+    {
+        _id: 4,
+        name: 'Chicken Noodle Salad',
+        description: 'Get ready to make all sorts of friends when you bring a big bowl of this Salad.',
+        chef: 'Chef John',
+        image: '/images/food4.jpg',
+        rating: 4.5,
+        numReviews: 15
+    },
+    {
+        _id: 5,
+        name: 'Watercress, Melon and Almond Salad',
+        description: 'Melons and watercress make great summer dessert salads.',
+        chef: 'Allrecipes Member',
+        image: '/images/food5.jpg',
+        rating: 4.5,
+        numReviews: 9
+    },
+    {
+        _id: 6,
+        name: 'Citrus-Herbed Baked Salmon',
+        description: 'This is one of the most flavorful and moist salmon recipes I have ever tasted.',
+        chef: 'cktb2793',
+        image: '/images/food6.jpg',
+        rating: 4,
+        numReviews: 10
+    },
+    {
+        _id: 7,
+        name: 'Insalata Caprese II',
+        description: 'Because this salad is so simple, fresh, top-quality tomatoes and mozzarella',
+        chef: 'Allrecipes Member',
+        image: '/images/food7.jpg',
+        rating: 5,
+        numReviews: 346
+    },
+    {
+        _id: 8,
+        name: 'Lemon-Vanilla French Toast',
+        description: 'This refreshing watermelon gazpacho is the perfect soup for summer.',
+        chef: 'Christa Talbott',
+        image: '/images/food8.jpg',
+        rating: 4,
+        numReviews: 2
+    },
+    {
+        _id: 9,
+        name: 'Easy Fried Cabbage',
+        description: 'An easy fried cabbage recipe that is on the table in 30 minutes.',
+        chef: 'CookingWithShelia',
+        image: '/images/food9.jpg',
+        rating: 5,
+        numReviews: 1
+    },
+    {
+        _id: 10,
+        name: 'Watermelon Feta Salad',
+        description: 'Sweet watermelon, spicy arugula, and salty feta cheese prove that contrasting',
+        chef: 'Allrecipes Member',
+        image: '/images/food10.jpg',
+        rating: 4.5,
+        numReviews: 20
+    },
+]
+
+export default foodList;
